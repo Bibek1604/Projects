df967d27e59658f710646e70791dfd464040c56d
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-28 12:30:42 +0545

    todo list created

---
+++ Projects/Todo List/script.js
@@ -0,0 +1,7 @@
+const inp = document.getElementById('inp');
+const text=document.querySelector('.text');
+function Add(){
+    if(inp.value===''){
+        alert("enter something to get idea")
+    }
+}
--- Projects/Todo List/scripts.js
+++ Projects/Todo List/scripts.js
@@ -1,32 +1,42 @@
+// const inp = document.getElementById('inp');
+// const text = document.querySelector('.text');
+
+// function Add() {
+//     if (inp.value === '') {
+//         alert("Enter your task list");
+//     } else {
+//         const newEle = document.createElement('li');
+//         newEle.textContent = inp.value;
+
+//         const removeBtn = document.createElement('span');
+//         removeBtn.innerHTML = '♻'; 
+//         removeBtn.className = 'cross';
+//         removeBtn.addEventListener('click', function () {
+//             newEle.remove();
+//             saveData();
+//         });
+
+//         newEle.appendChild(removeBtn);
+//         text.appendChild(newEle);
+//         inp.value = '';
+//         saveData();
+//     }
+
+// }
+// dust
+
+
 const inp = document.getElementById('inp');
 const text = document.querySelector('.text');
 
-function Add() {
-    if (inp.value === '') {
-        alert("Enter your task list");
-    } else {
-        const newEle = document.createElement('li');
-        newEle.textContent = inp.value;
-
-        const removeBtn = document.createElement('span');
-        removeBtn.innerHTML = '&#10006;'; // Unicode for "cross" symbol
-        removeBtn.className = 'cross';
-        removeBtn.addEventListener('click', function () {
-            newEle.remove();
-            saveData();
-        });
-
-        newEle.appendChild(removeBtn);
-        text.appendChild(newEle);
-        inp.value = '';
-        saveData();
-    }
+function add(){
+    add.eventListner=('click',functionn())
+    if(inp.value===''){
+        alert("please enter the value")
+    }else
+    {const newEle=document.createElement('li');
+    newEle.innerHTML=inp.value;
+    text.append(newEle);
+    inp.value='';
 }
-
-function saveData() {
-    localStorage.setItem('text', text.innerHTML);
-}
-
-function showTask() {
-    text.innerHTML = localStorage.getItem('text');
 }
