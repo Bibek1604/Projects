afe4471b0105c358e0afec2749126aaa20efcce3
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-28 13:43:54 +0545

    github logo added

--- Projects/Todo List/scripts.js
+++ Projects/Todo List/scripts.js
@@ -1,42 +1,19 @@
-// const inp = document.getElementById('inp');
-// const text = document.querySelector('.text');
-
-// function Add() {
-//     if (inp.value === '') {
-//         alert("Enter your task list");
-//     } else {
-//         const newEle = document.createElement('li');
-//         newEle.textContent = inp.value;
-
-//         const removeBtn = document.createElement('span');
-//         removeBtn.innerHTML = '♻'; 
-//         removeBtn.className = 'cross';
-//         removeBtn.addEventListener('click', function () {
-//             newEle.remove();
-//             saveData();
-//         });
-
-//         newEle.appendChild(removeBtn);
-//         text.appendChild(newEle);
-//         inp.value = '';
-//         saveData();
-//     }
-
-// }
-// dust
-
-
 const inp = document.getElementById('inp');
 const text = document.querySelector('.text');
 
-function add(){
-    add.eventListner=('click',functionn())
-    if(inp.value===''){
-        alert("please enter the value")
-    }else
-    {const newEle=document.createElement('li');
-    newEle.innerHTML=inp.value;
-    text.append(newEle);
-    inp.value='';
+function Add() {
+    if (inp.value === '') {
+        alert("Please Enter the Text");
+    } else {
+        const newEl = document.createElement('li');
+        newEl.innerHTML = `${inp.value} <span class="cross">&#10006;</span>`; // Unicode for "cross" symbol
+
+        text.appendChild(newEl);
+        inp.value = '';
+        newEl.querySelector('.cross').addEventListener('click', remove);
+    }
 }
+
+function remove() {
+    this.parentElement.remove();
 }
